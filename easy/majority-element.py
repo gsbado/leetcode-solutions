d44fdcd30f5f766d6af8c90b6e201e29de41772b
gsbado <gabibado92@gmail.com>
2026-03-11 21:46:31 -0300

    New solution to majority-element

--- easy/majority-element.py
+++ easy/majority-element.py
@@ -2,11 +2,5 @@
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        countNumbers = {}
-        for num in nums:
-            if num in countNumbers:
-                countNumbers[num] += 1
-            else:
-                countNumbers[num] = 1
-            if countNumbers[num] > (len(nums)/2):
-                return num
+        nums.sort()
+        return nums[len(nums)//2]
